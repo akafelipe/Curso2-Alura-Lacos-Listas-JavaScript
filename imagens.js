f5bcc886cc7c77074818c31953772df9385b9026
Felipe Siqueira <sqr.felipe@gmail.com>
2023-08-30 11:04:15 -0300

    Create imagens.js

---
+++ imagens.js
@@ -0,0 +1,15 @@
+//IMAGENS
+
+let ator;
+let fundo;
+let carro1;
+let carro2;
+let carro3;
+
+function preload(){
+  fundo = loadImage("imagens/estrada.png")
+  ator = loadImage("imagens/ator-1.png")
+  carro1 = loadImage("imagens/carro-1.png")
+  carro2 = loadImage("imagens/carro-2.png")
+  carro3 = loadImage("imagens/carro-3.png")
+}
