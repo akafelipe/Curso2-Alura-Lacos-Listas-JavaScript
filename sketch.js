16d2528f64273c0e0d3b940de5e0851552b70ff7
Felipe Siqueira <sqr.felipe@gmail.com>
2023-08-30 11:05:53 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup(){
+  createCanvas(500, 400);
+}
+
+function draw() {
+  background(fundo);
+  mostraAtor();
+  movimentaAtor();
+  mostraCarro1();
+  movimentaCarro1();
+  voltaPosCarro();
+}
+
