922c13c3936022bf8b8ae67e13f89f1327c49e68
Felipe Siqueira <sqr.felipe@gmail.com>
2023-08-31 09:55:53 -0300

    Update imagens.js

--- imagens.js
+++ imagens.js
@@ -1,15 +1,26 @@
-//IMAGENS
+// imagens e sons do jogo
 
-let ator;
-let fundo;
-let carro1;
-let carro2;
-let carro3;
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
+//sons do jogo
+
+let somDaTrilha;
+let somDaColisao;
+let somDoPonto;
 
 function preload(){
-  fundo = loadImage("imagens/estrada.png")
-  ator = loadImage("imagens/ator-1.png")
-  carro1 = loadImage("imagens/carro-1.png")
-  carro2 = loadImage("imagens/carro-2.png")
-  carro3 = loadImage("imagens/carro-3.png")
+  imagemDaEstrada = loadImage("imagens/estrada.png");
+  imagemDoAtor = loadImage("imagens/ator-1.png");
+  imagemCarro = loadImage("imagens/carro-1.png");
+  imagemCarro2 = loadImage("imagens/carro-2.png");
+  imagemCarro3 = loadImage("imagens/carro-3.png");  
+  imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3]
+  
+  somDaTrilha = loadSound("sons/trilha.mp3");
+  somDaColisao = loadSound("sons/colidiu.mp3");
+  somDoPonto = loadSound("sons/pontos.wav");
 }
