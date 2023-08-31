14a4c4cc8a96ae455203221ce867eaca8d58a741
Felipe Siqueira <sqr.felipe@gmail.com>
2023-08-31 09:47:16 -0300

    Update ator.js

--- ator.js
+++ ator.js
@@ -1,9 +1,11 @@
-//Posicao ator
+//código do ator
+let xAtor = 85;
 let yAtor = 366;
-let xAtor = 100;
+let colisao = false;
+let meusPontos = 0;
 
 function mostraAtor(){
-  image(ator, xAtor, yAtor, 30, 30) 
+  image(imagemDoAtor, xAtor, yAtor, 30, 30);
 }
 
 function movimentaAtor(){
@@ -12,5 +14,42 @@ function movimentaAtor(){
   }
   if (keyIsDown(DOWN_ARROW)){
     yAtor += 3;
-  }  
+  }
 }
+
+function verificaColisao(){
+  //collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
+  for (let i = 0; i < imagemCarros.length; i++){
+    colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15)
+    if (colisao){
+      voltaAtorParaPosicaoInicial();
+    }
+  }
+}
+
+function voltaAtorParaPosicaoInicial(){
+  yAtor = 366;
+}
+
+function incluiPontos(){
+  textAlign(CENTER);
+  textSize(25);
+  fill(color(255, 240, 60))
+  text(meusPontos, width / 5, 27);
+}
+
+function marcaPonto(){
+  if (yAtor < 15){
+    meusPontos += 1;
+    voltaAtorParaPosicaoInicial();
+  }
+}
+
+
+
+
+
+
+
+
+
